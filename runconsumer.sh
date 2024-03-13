92165191b1d3cdb93a675d198656a07c0c2af265
lapentafd <francesco.lapenta@est.tech>
2024-03-13 10:36:34 +0000

    Removing old scripts
    Signed-off-by: lapentafd <francesco.lapenta@est.tech>

--- runconsumer.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo compile...
-
-# pass <topicName> <numOfRecsToProduce> as args
-
-mvn -q clean compile exec:java \
- -Dexec.mainClass="com.demo.kafka.Application" \
- -Dexec.args="consumer $1"
